858a652a125c132a67c7c94ce630b95bdff1b022
Xorse Wisdom <wisdomxorse928@gmail.com>
2025-05-16 14:07:34 +0000

    backend model commit

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
     _id: {type: String, required: true},
     name: {type: String, required: true},
-    email: {type: String, required: true, unique},
+    email: {type: String, required: true},
     image: {type: String, required: true},
     role: {type: String, enum:["user", "hotelOwner"], default: "user"},
     recentSearch: [{type: String, required: true}],
