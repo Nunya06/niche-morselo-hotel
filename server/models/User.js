f3812e98aed6c43f4236d0b6e7d7c522c86a005e
Xorse Wisdom <wisdomxorse928@gmail.com>
2025-05-16 12:26:21 +0000

    Commit with backend

---
+++ server/models/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    _id: {type: String, required: true},
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique},
+    image: {type: String, required: true},
+    role: {type: String, enum:["user", "hotelOwner"], default: "user"},
+    recentSearch: [{type: String, required: true}],
+},{timestamps:true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+// Clerk middleare
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebHooks from "./controllers/clerkWebhooks.js";
+
+// Connection function 
+connectDB()
+
+const app = express()
+app.use(cors()) //Enable cross-origin sharing
+
+// Middleware
+app.use(express.json())
+app.use(clerkMiddleware())
+
+
+
+// API to listen to clerkWebhooks
+app.subscribe("/api/clerk", clerkWebHooks)
+
+app.get('/', (req, res)=> res.send("API is working"))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
